0fc25c056705251f9830cbeca141db1463b1cb5c
Babenko Liudmyla <savelieva0509@gmail.com>
2024-01-16 01:12:54 +0200

    add router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,20 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Router, Route, Routes } from 'react-router-dom';
+import AccountsPage from 'pages/AccountsPage/AccountsPage';
+import ProfilesPage from 'pages/ProfilesPage/ProfilesPage';
+import CampaignsPage from 'pages/CampaignsPage/CampaignsPage';
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<AccountsPage />} />
+        <Route path="/accounts/:accountId/profiles" element={<ProfilesPage />} />
+        <Route path="/profiles/:profileId/campaigns" element={<CampaignsPage />} />
+      </Routes>
+   </> 
+     
+
+  );
+}
+export default App;

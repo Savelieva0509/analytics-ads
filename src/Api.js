e05ee5d79d0b760839078654787a0ba59e53f6e3
Babenko Liudmyla <savelieva0509@gmail.com>
2024-01-16 11:02:36 +0200

    fix

--- src/Api.js
+++ src/Api.js
@@ -1,12 +1,48 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://6571f80ad61ba6fcc0141958.mockapi.io';
+const BASE_URL = 'https://6571f80ad61ba6fcc0141958.mockapi.io';
 
-export const getTotalAccounts = async () => {
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const getAccounts = async () => {
   try {
-    const { data } = await axios.get(`/accounts`);
-    return data;
+    const response = await api.get('/accounts');
+    console.log('Received accounts:', response.data);
+    return response;
   } catch (error) {
-    console.error('Error getting total accounts:', error);
+    console.error('Error fetching accounts:', error);
     throw error;
   }
 };
+
+export const getProfiles = async accountId => {
+  try {
+    const response = await api.get(`/accounts/${accountId}/profiles`);
+    console.log('Received profiles:', response.data);
+    return response;
+  } catch (error) {
+    console.error('Error fetching profiles:', error);
+    throw error;
+  }
+};
+
+
+export const getCampaigns = async profileId => {
+  try {
+    const response = await api.get(`/profiles/${profileId}/campaigns`);
+    console.log('Received campaigns:', response.data);
+    return response;
+  } catch (error) {
+    console.error('Error fetching campaigns:', error);
+    throw error;
+  }
+};
+
+const apiService = {
+  getAccounts,
+  getProfiles,
+  getCampaigns,
+};
+
+export default apiService;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import AccountsPage from 'pages/AccountsPage/AccountsPage';
 import ProfilesPage from 'pages/ProfilesPage/ProfilesPage';
 import CampaignsPage from 'pages/CampaignsPage/CampaignsPage';
@@ -9,12 +9,16 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<AccountsPage />} />
-        <Route path="/accounts/:accountId/profiles" element={<ProfilesPage />} />
-        <Route path="/profiles/:profileId/campaigns" element={<CampaignsPage />} />
+        <Route
+          path="/accounts/:accountId/profiles"
+          element={<ProfilesPage />}
+        />
+        <Route
+          path="/profiles/:profileId/campaigns"
+          element={<CampaignsPage />}
+        />
       </Routes>
-   </> 
-     
-
+    </>
   );
 }
 export default App;
